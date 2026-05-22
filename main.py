72afe7c1aee67ede83933ab8924e23a24721b236
REGGDIS <regdis@gmail.com>
2026-05-22 16:21:48 -0400

    Versión estable funcional con ExifTool y lectura/escritura de etiquetas

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class EtiquetadorApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Etiquetador de Imágenes")
-        self.root.geometry("800x600")
+        self.root.geometry("800x700")
 
         self.exiftool_path = os.path.join(
             os.path.dirname(__file__), "exiftool.exe")
